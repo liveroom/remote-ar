7e1ad90eed90e000ef7a2938e105774e812ebcb5
Poorna Perera <poorna@liveroom.xyz>
2019-09-30 11:42:00 +0530

    Added back buttons functionalities and changed UIs in the application.

---
+++ RemoteAR/Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitApplication : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Application quit
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+
+    // Back to start view
+    public void GotoStartView()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+}
